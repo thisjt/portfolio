75e30c8b198e4f3d6fd6746b571791e0f53d0321
Joshua Tahanlangit <contact@thisjt.me>
2024-04-08 13:30:31 +0800

    re script workflow

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			trailingSlash: 'always',
 			fallback: '404.html'
 		}),
 		paths: {
